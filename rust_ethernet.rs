0a886b770334ce20bc7d8e7557baff9a4d3db3ab
yosramadiouni123 <121777218+yosramadiouni123@users.noreply.github.com>
2024-05-21 17:02:27 +0100

    Update rust_ethernet.rs

--- rust_ethernet.rs
+++ rust_ethernet.rs
@@ -43,7 +43,7 @@ pub fn connect(address: &SocketAddr) -> Result<TcpStream> {
 }
 
 
-pub fn send_data(stream: &TcpStream, data: [u8; 13]) -> Result<usize> {
+pub fn send_data(stream: &TcpStream, data: [u8; 52]) -> Result<usize> {
     // Ensure the data vector has exactly 13 elements
     
     
@@ -71,7 +71,7 @@ pub struct RustClientTest {
 impl kernel::Module for RustClientTest {
     fn init(_name: &'static CStr, _module: &'static ThisModule) -> Result<Self> {
         let remote_addr = SocketAddr::V4(SocketAddrV4::new(Ipv4Addr::ANY, 8080)) ;
-        pr_info!("---INFO--- CREATED SOCKET SUCCCESSFULLY !") ; 
+        pr_info!("---INFO--- CREATED SOCKET SUCCESSFULLY !") ; 
         coarse_sleep(Duration::from_secs(1)) ;
         
         let stream = connect(&remote_addr)?;
@@ -79,18 +79,23 @@ impl kernel::Module for RustClientTest {
         coarse_sleep(Duration::from_secs(1)) ;
         // Example number to send 
         
-        let raw_data:[u8;13]= [
+        let raw_data:[u8;52]= [
             //can_id
-            0x00, 0x11, 0x22 , 0x33,
-            //can_dlc
+            0x00, 0x00, 0x22 , 0x33,
+            //can_len
             0x44,
+            //flags
+            0x11,
             //data
-            0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x22, 0x33,
+            0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x00, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 
+            0x33, 0x44, 0x55, 0x66, 0xAA, 0xBB, 0xCC, 0xDD, 0xEE, 0xFF, 0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x00,0x22, 0x33, 0x44, 0x55, 0x00, 
+
+
         ];          
        
         let data_vec=raw_data; 
        
-        pr_info!("---INFO--- PUSHING DATA (CAN FRAME) TO THE GATEWAY ! ") ;
+        pr_info!("---INFO--- PUSHING DATA (CANFD FRAME) TO THE GATEWAY ! ") ;
         coarse_sleep(Duration::from_secs(1)) ;
         send_data(&stream, data_vec)? ; 
 
