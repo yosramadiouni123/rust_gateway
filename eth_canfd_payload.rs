37faf48c71f1ef23446d98457f1a567ff40ed49d
yosramadiouni123 <121777218+yosramadiouni123@users.noreply.github.com>
2024-05-24 18:36:07 +0100

    Update eth_canfd_payload.rs

--- eth_canfd_payload.rs
+++ eth_canfd_payload.rs
@@ -1,16 +1,7 @@
 /// Different imports : 
-use bindings::canid_t;
+use bindings::{canfd_frame, canid_t};
 use crate::prelude::*;
 use alloc::borrow::ToOwned;
-
-
-
-
-
-/// This struct defines some of the fields of the canfd_frame
-/// Could look at a potential bindgen use 
-/// This struct defines some of the fields of the canfd_frame
-/// Could look at a potential bindgen use 
 #[derive(PartialEq)]
 #[derive(Clone)]
 pub struct canfdFrame {
@@ -34,15 +25,40 @@ impl canfdFrame {
     pub fn get_data(&self) -> &[u8; 46] {
         &self.data
     }
+    pub fn deserialize_canfd_payload(buffer: &[u8]) -> Option<Self> {
+       
+        // Extract individual field slices using nom library
+        let (can_id, rest) = buffer.split_at(4);
+        let (len, rest) = rest.split_at(1);
+        let (flags, rest) =rest.split_at(1);
+        let (data, _) = rest.split_at(46);
+        let mut can_id_arr = [0; 4];
+        can_id_arr.copy_from_slice(can_id);
+        let mut len_arr = [0; 1];
+        len_arr.copy_from_slice(len);
+        let mut flags_arr = [0; 1];
+        flags_arr.copy_from_slice(flags);
+        let data_vec = data.to_owned();
+        let data_v1=vec_to_array1(data_vec) ; 
+        // Deserialize individual fields
+       // let data = Ethloadfd::from_bytes(data)?;
+        //let can_id=u32::from_be_bytes(can_id);
+        let can_id_vec = can_id.to_owned();
+        let can_id_v1=vec_to_array(can_id_vec) ; 
+        let can_id = u32::from_be_bytes(can_id_arr) ; 
+        let len = u8:: from_be_bytes(len_arr);
+        let flags= u8::from_be_bytes(flags_arr);
+        //let data_vec= data.to_o
+        Some(Self {
+            can_id,
+            len:len,
+            flags:flags,
+            data: data_v1.unwrap(),
+        })
+    }
 }
 
-/*fn serialize_canfd_frame(frame: &canfdFrame) -> Vec<u8> {
-    let mut serialized_data = Vec::new();
-    let mut serializer = serde_cbor::Serializer::new(&mut serialized_data);
-    frame.serialize(&mut serializer).unwrap();
-    serialized_data
 
-}*/
 
 #[derive(PartialEq)]
 #[derive(Clone)]
@@ -70,7 +86,7 @@ pub struct TcpFrame {
 #[derive(Clone)]
 pub struct Ipv4Frame {
     pub header: Ipv4Header,
-    pub data: TcpFrame,
+    pub datatcp: TcpFrame,
 }
 
 /// EthFrame struct
@@ -94,7 +110,7 @@ impl EthFrame {
     }
     pub fn get_tcp_header(&self) -> Option<&TcpHeader> {
         if self.ethertype == EtherTypefd::IPV4 {
-            Some(&self.data.data.tcphdr)
+            Some(&self.data.datatcp.tcphdr)
         } else {
             None
         }
@@ -204,7 +220,7 @@ pub struct Ethloadfd {
    /// tcphdr 
    pub tcphdr : TcpHeader ,
    /// data 
-   pub data: [u8;4],
+   pub data: [u8;46],
 }
 //==========serialize Ethloadfd ============
 
@@ -253,90 +269,111 @@ pub struct EthCanfdpayLoad {
 
 
 impl EthCanfdpayLoad {
-    pub fn to_eth_frame(payload: EthCanfdpayLoad) -> EthFrame {
+    pub fn to_eth_frame(payload:  &canfdFrame) -> Self {
         let ip_header = Ipv4Header {
-            version: payload.data.iphdr.version,
-            len: payload.data.iphdr.len,
-            to_s: payload.data.iphdr.to_s,
-            total_len: payload.data.iphdr.total_len,
-            id: payload.data.iphdr.id,
-            flags: payload.data.iphdr.flags,
-            frag_offset: payload.data.iphdr.frag_offset,
-            ttl: payload.data.iphdr.ttl,
-            protocol: payload.data.iphdr.protocol,
-            checksum: payload.data.iphdr.checksum,
-            src: payload.data.iphdr.src,
-            dst: payload.data.iphdr.dst,
+            version: 4,
+            len: 20, // Assuming minimum header length
+            to_s: 1, // ToS (Type of Service) field example
+            total_len: 50, // Assuming total length (including data)
+            id: 100, // Example ID
+            flags: 0, // Flags field example (no flags set)
+            frag_offset: 0, // Fragment offset (no fragmentation)
+            ttl: 64, // Time to live example
+            protocol: 6, // TCP protocol
+            checksum: 0xABCD, // Mock checksum value
+            src: [192, 168, 1, 10],
+            dst: [10, 0, 0, 1],
         };
         let tcp_header = TcpHeader {
-            src_port: payload.data.tcphdr.src_port,
-            dst_port: payload.data.tcphdr.dst_port,
-            seq: payload.data.tcphdr.seq,
-            ack: payload.data.tcphdr.ack,
-            offset: payload.data.tcphdr.offset,
-            reserved: payload.data.tcphdr.reserved,
-            flags: payload.data.tcphdr.flags,
-            window: payload.data.tcphdr.window,
-            checksum: payload.data.tcphdr.checksum,
-            urgent_ptr: payload.data.tcphdr.urgent_ptr,
+            src_port: 8080,
+            dst_port: 4433,
+            seq: 1234567890,
+            ack: 987654321,
+            offset: 5, // Assuming header length (5 words)
+            reserved: 0, // Reserved bits (set to 0)
+            flags: 0, // Flags field example (no flags set)
+            window: 1024, // Example window size
+            checksum: 0x1234, // Mock checksum value
+            urgent_ptr: 0, // Urgent pointer (not used here)
         };
         let mut data = Vec::new();
-        data.try_extend_from_slice(&payload.data.data);
-        let eth_frame = EthFrame {
-            dst: [0x00, 0x11, 0x22, 0x33, 0x44, 0x55],
-            src: [0x00, 0x34, 0x56, 0x78, 0x90, 0xAB],
-            ethertype: payload.ethertype,
-            data: Ipv4Frame {
-                header: ip_header,
-                data: TcpFrame {
-                    tcphdr: tcp_header,
-                    data:payload.data.data,
-                },
+        data.try_extend_from_slice(&payload.data);
+        let eth_frame = EthCanfdpayLoad {
+            dst_mac: [0x00, 0x11, 0x22, 0x33, 0x44, 0x55],
+            src_mac: [0x00, 0x34, 0x56, 0x78, 0x90, 0xAB],
+            ethertype: EtherTypefd::IPV4,
+            data: Ethloadfd {
+                iphdr: ip_header,
+                tcphdr: tcp_header,
+                data:payload.data,
+                
             },        
         };
         eth_frame
     }
+        pub fn deserialize_eth_canfd_payload(buffer: &[u8] ) -> Option<EthCanfdpayLoad>
+        {   // Extract individual field slices using nom library
+            let (dst_mac, rest) = buffer.split_at(6);
+            let (src_mac, rest) = rest.split_at(6);
+            let (ethertype, rest) =rest.split_at(2);
+            let (data, _) = rest.split_at(88);
+            let mut dst_arr = [0; 6];
+            dst_arr.copy_from_slice(dst_mac);
 
-    pub fn deserialize_eth_payload(buffer: &[u8]) -> Option<Self> {
-       
-        // Extract individual field slices using nom library
-        let (dst_mac, rest) = buffer.split_at(6);
-        let (src_mac, rest) = rest.split_at(6);
-        let (ethertype, rest) =rest.split_at(2);
-        let (data_can_bytes, _) = rest.split_at(46);
-        let mut dst_arr = [0; 6];
-        dst_arr.copy_from_slice(dst_mac);
-        let mut eth_arr = [0; 2];
-        eth_arr.copy_from_slice(ethertype);
-        let mut src_arr = [0; 6];
-        src_arr.copy_from_slice(src_mac);
-        // Deserialize individual fields
-        let data_can = Ethloadfd::from_bytes(data_can_bytes)?;
-        Some(Self {
-            dst_mac: dst_arr, // Convert slice to owned array
-            src_mac: src_arr,
-            ethertype: EtherTypefd(u16::from_be_bytes(eth_arr)), // Assuming conversion from u16
-            data: data_can,
-        })
+            let mut src_arr = [0; 6];
+            src_arr.copy_from_slice(src_mac);
+    
+            let mut eth_arr = [0; 2];
+            eth_arr.copy_from_slice(ethertype);
+    
+            
+
+            let mut data_arr = [0; 88];
+            data_arr.copy_from_slice(data);
+            let data_vec = data.to_owned();
+           // let data_v1=vec_to_array1(data_vec) ; 
+            // Deserialize individual fields
+            let data = Ethloadfd::from_bytes(data)?;
+            //let can_id=u32::from_be_bytes(can_id);
+            //let can_id_vec = can_id.to_owned();
+            //let can_id_v1=vec_to_array(can_id_vec) ; 
+           // let can_id = u32::from_be_bytes(can_id_arr) ; 
+            //let len = u8:: from_be_bytes(len_arr);
+          //  let flags= u8::from_be_bytes(flags_arr);
+            //let data_vec= data.to_o
+            Some(Self {
+                dst_mac: dst_arr, // Convert slice to owned array
+                src_mac: src_arr,
+                ethertype: EtherTypefd(u16::from_be_bytes(eth_arr)), // Assuming conversion from u16
+                data: data,
+
+            })  
+        }
+
+        
+    
+    
+    
+}
+    pub fn serialize_eth_canfd_payload(payload: &EthCanfdpayLoad) -> Vec<u8> {
+        let mut serialized_data = Vec::new();
+        // Serialize dst_mac
+        serialized_data.try_extend_from_slice(&payload.dst_mac);
+        // Serialize src_mac
+        serialized_data.try_extend_from_slice(&payload.src_mac);
+        // Serialize ethertype (assuming u16 representation)
+        let ethertype_bytes = (payload.ethertype.0 as u16).to_be_bytes();
+        serialized_data.try_extend_from_slice(&ethertype_bytes);
+        // Serialize data (using Ethloadfd serialization function)
+        let ethload_bytes = serialize_ethload(&payload.data);
+        serialized_data.try_extend_from_slice(&ethload_bytes);
+        serialized_data
     }
-}
 
 
 
-pub fn serialize_eth_canfd_payload(payload: &EthCanfdpayLoad) -> Vec<u8> {
-    let mut serialized_data = Vec::new();
-    // Serialize dst_mac
-    serialized_data.try_extend_from_slice(&payload.dst_mac);
-    // Serialize src_mac
-    serialized_data.try_extend_from_slice(&payload.src_mac);
-    // Serialize ethertype (assuming u16 representation)
-    let ethertype_bytes = (payload.ethertype.0 as u16).to_be_bytes();
-    serialized_data.try_extend_from_slice(&ethertype_bytes);
-    // Serialize data (using Ethloadfd serialization function)
-    let ethload_bytes = serialize_ethload(&payload.data);
-    serialized_data.try_extend_from_slice(&ethload_bytes);
-    serialized_data
-}
+
+
 
 
 
@@ -377,22 +414,18 @@ pub fn serialize_tcp_header(tcp_header: &TcpHeader) -> Vec<u8> {
 
 impl Ethloadfd {
     pub fn from_bytes(buffer: &[u8]) -> Option<Ethloadfd> {
-        // Check if the buffer has enough data for all fields
-        /*if buffer.len() < mem::size_of::<Ipv4Header>() + mem::size_of::<TcpHeader>() + mem::size_of::<[u8; 4]>() {
-            return None;
-        }*/
-
+       
         // Extract individual field slices
         pr_info!("{}", buffer.len());
         let (iphdr_bytes, rest) = buffer.split_at(21);
         let (tcphdr_bytes, rest) = rest.split_at(21);
-        let (data, _) = rest.split_at(4);
+        let (data, _) = rest.split_at(46);//46
 
         // Deserialize individual fields
         let iphdr = Ipv4Header::deserialize_ip_header(iphdr_bytes)?;
         let tcphdr = TcpHeader::deserialize_tcp_header(tcphdr_bytes)?;
         let data_vec = data.to_owned();
-        let data_v1=vec_to_array(data_vec) ; 
+        let data_v1=vec_to_array1(data_vec) ; 
 
 
         Some(Ethloadfd{
@@ -406,10 +439,7 @@ impl Ethloadfd {
 
 impl Ipv4Header {
     pub fn deserialize_ip_header(buffer: &[u8]) -> Option<Ipv4Header> {
-            // Check if the buffer has enough data (expected size)
-            /*if buffer.len() < mem::size_of::<Ipv4Header>() {
-                return None;
-            }*/
+            
         
             // Extract individual field values
             let (version, rest) = buffer.split_at(1);
@@ -461,59 +491,59 @@ impl Ipv4Header {
             })
         }
     }
-    impl TcpHeader {
-        pub fn deserialize_tcp_header(buffer: &[u8]) -> Option<TcpHeader> {
+impl TcpHeader {
+    pub fn deserialize_tcp_header(buffer: &[u8]) -> Option<TcpHeader> {
     
-            // Extract individual field slices
-            let (src_port, rest) = buffer.split_at(2);
-            let (dst_port, rest) = rest.split_at(2);
-            let (seq, rest) = rest.split_at(4);
-            let (ack, rest) = rest.split_at(4);
-            let (offset, rest) = rest.split_at(1);
-            let (reserved,rest) =rest.split_at(1) ; 
-            let (flags, rest) = rest.split_at(1);
-            let (window, rest) = rest.split_at(2);
-            let (checksum, rest) = rest.split_at(2);
-            let (urgent_ptr, _) = rest.split_at(2);
+        // Extract individual field slices
+        let (src_port, rest) = buffer.split_at(2);
+        let (dst_port, rest) = rest.split_at(2);
+        let (seq, rest) = rest.split_at(4);
+        let (ack, rest) = rest.split_at(4);
+        let (offset, rest) = rest.split_at(1);
+        let (reserved,rest) =rest.split_at(1) ; 
+        let (flags, rest) = rest.split_at(1);
+        let (window, rest) = rest.split_at(2);
+        let (checksum, rest) = rest.split_at(2);
+        let (urgent_ptr, _) = rest.split_at(2);
         
-            // Convert slices to fixed-size arrays before conversion
-            let mut src_port_arr = [0; 2];
-            src_port_arr.copy_from_slice(src_port);
-            let mut dst_port_arr = [0; 2];
-            dst_port_arr.copy_from_slice(dst_port);
-            let mut seq_arr = [0; 4];
-            seq_arr.copy_from_slice(seq);
-            let mut ack_arr = [0; 4];
-            ack_arr.copy_from_slice(ack);
-            let mut window_arr = [0; 2];
-            window_arr.copy_from_slice(window);
-            let mut checksum_arr = [0; 2];
-            checksum_arr.copy_from_slice(checksum);
-            let mut urgent_ptr_arr = [0; 2];
-            urgent_ptr_arr.copy_from_slice(urgent_ptr);
+        // Convert slices to fixed-size arrays before conversion
+        let mut src_port_arr = [0; 2];
+        src_port_arr.copy_from_slice(src_port);
+        let mut dst_port_arr = [0; 2];
+        dst_port_arr.copy_from_slice(dst_port);
+        let mut seq_arr = [0; 4];
+        seq_arr.copy_from_slice(seq);
+        let mut ack_arr = [0; 4];
+        ack_arr.copy_from_slice(ack);
+        let mut window_arr = [0; 2];
+        window_arr.copy_from_slice(window);
+        let mut checksum_arr = [0; 2];
+        checksum_arr.copy_from_slice(checksum);
+        let mut urgent_ptr_arr = [0; 2];
+        urgent_ptr_arr.copy_from_slice(urgent_ptr);
         
-            // Extract individual fields and convert from network byte order
-            let src_port = u16::from_be_bytes(src_port_arr);
-            let dst_port = u16::from_be_bytes(dst_port_arr);
-            let seq = u32::from_be_bytes(seq_arr);
-            let ack = u32::from_be_bytes(ack_arr);
-            let offset = (offset[0] >> 4) & 0x0F;
-            let reserved = offset & 0x0F;
-            let flags = flags[0];
-            let window = u16::from_be_bytes(window_arr);
-            let checksum = u16::from_be_bytes(checksum_arr);
-            let urgent_ptr = u16::from_be_bytes(urgent_ptr_arr);
+        // Extract individual fields and convert from network byte order
+        let src_port = u16::from_be_bytes(src_port_arr);
+        let dst_port = u16::from_be_bytes(dst_port_arr);
+        let seq = u32::from_be_bytes(seq_arr);
+        let ack = u32::from_be_bytes(ack_arr);
+        let offset = (offset[0] >> 4) & 0x0F;
+        let reserved = offset & 0x0F;
+        let flags = flags[0];
+        let window = u16::from_be_bytes(window_arr);
+        let checksum = u16::from_be_bytes(checksum_arr);
+        let urgent_ptr = u16::from_be_bytes(urgent_ptr_arr);
         
-            // Create the TcpHeader struct
-            Some(TcpHeader{
-                src_port,
-                dst_port,
-                seq,
-                ack,
-                offset,
-                reserved,
-                flags,
-                window,
+        // Create the TcpHeader struct
+        Some(TcpHeader{
+            src_port,
+            dst_port,
+            seq,
+            ack,
+            offset,
+            reserved,
+            flags,
+             window,
                 checksum,
                 urgent_ptr,
             })
@@ -521,16 +551,42 @@ impl Ipv4Header {
     }    
     
 
-    fn vec_to_array(slice_data: &[u8]) -> core::result::Result<[u8; 4], &'static str> {
-        // Check if the slice has exactly 4 elements
-        if slice_data.len() != 4 {
-            return Err("Error: Slice must contain exactly 4 elements");
-        }
-    
-        // Safely convert the slice to a fixed-size array
-        unsafe {
-            // This is safe because we checked the slice length beforehand
-            let array: [u8; 4] = *(slice_data.as_ptr() as *const [u8; 4]);
-            Ok(array)
-        }
+fn vec_to_array1(slice_data: &[u8]) -> core::result::Result<[u8; 46], &'static str> {
+    // Check if the slice has exactly 4 elements
+    if slice_data.len() != 46{
+        return Err("Error: Slice must contain exactly 46 elements");
     }
+    
+    // Safely convert the slice to a fixed-size array
+    unsafe {
+        // This is safe because we checked the slice length beforehand
+        let array: [u8; 46] = *(slice_data.as_ptr() as *const [u8; 46]);
+        Ok(array)
+    }
+}
+fn vec_to_array(slice_data: &[u8]) -> core::result::Result<[u8; 4], &'static str> {
+    // Check if the slice has exactly 4 elements
+    if slice_data.len() != 4{
+        return Err("Error: Slice must contain exactly 4 elements");
+    }
+    
+    // Safely convert the slice to a fixed-size array
+    unsafe {
+        // This is safe because we checked the slice length beforehand
+        let array: [u8; 4] = *(slice_data.as_ptr() as *const [u8; 4]);
+        Ok(array)
+    }
+}
+fn vec_to_array2(slice_data: &[u8]) -> core::result::Result<[u8; 1], &'static str> {
+    // Check if the slice has exactly1 elements
+    if slice_data.len() != 1{
+        return Err("Error: Slice must contain exactly 1 elements");
+    }
+    
+    // Safely convert the slice to a fixed-size array
+    unsafe {
+        // This is safe because we checked the slice length beforehand
+        let array: [u8; 1] = *(slice_data.as_ptr() as *const [u8; 1]);
+        Ok(array)
+    }
+}
